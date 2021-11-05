2bd0783b4582fa6a01af5b419940a58a09f6b3d1
ryoono <ono@pluslab.org>
2021-11-06 01:14:03 +0900

    leaderDev -> followerDevへの通信成功版

--- src/followerDev/followerDev.ino
+++ src/followerDev/followerDev.ino
@@ -1,9 +1,148 @@
-void setup() {
-  // put your setup code here, to run once:
+/*#include <ESP8266WiFi.h>
+//#include <WiFiClient.h> 
+#include <WiFiUDP.h>
 
+#include "wifiConfig.h"
+
+#define LED_PIN 4
+#define LED2_PIN 2
+
+char WiFibuff[16];
+int led_state = LOW;
+
+// UDP setting
+WiFiUDP UDP_Tx;
+WiFiUDP UDP_Rx;
+
+// WiFi settings
+IPAddress myIP( FOLLOWER_IP_ADDRESS_1 );
+IPAddress mySubnet( MY_SUBNETMASK );
+IPAddress leaderIpAddress1( LEADER_IP_ADDRESS );
+const char *ssid = MY_SSID;
+const char *password = MY_SSID_PASSWORD;
+
+int flag = 0;
+
+void rcvWiFi() {
+  UDP_Rx.read(WiFibuff, 1);
+  UDP_Rx.flush();
+}
+
+void connectWiFi() {
+  
+  WiFi.begin( ssid, password);
+  WiFi.config( myIP, WiFi.gatewayIP(), mySubnet);
+
+  while( WiFi.status() != WL_CONNECTED ){
+    delay(500);
+    led_state = !led_state;
+    digitalWrite( LED_PIN, led_state);
+  }
+  
+  digitalWrite( LED_PIN, LOW);
+
+  UDP_Tx.begin( FOLLOWER_PORT_TX );
+  UDP_Rx.begin( FOLLOWER_PORT_RX );
+}
+
+void setup() {
+
+  pinMode( LED_PIN, OUTPUT);
+  digitalWrite( LED_PIN, led_state);
+  pinMode( LED2_PIN, OUTPUT);
+  digitalWrite( LED2_PIN, LOW);
+  
+  WiFi.mode( WIFI_STA );
+  connectWiFi();
+}
+
+void sendWiFi(char byteData[]) {
+  
+  if ( UDP_Tx.beginPacket( leaderIpAddress1, LEADER_PORT_RX1) ) {
+    UDP_Tx.write(byteData);
+    UDP_Tx.endPacket();
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  if( UDP_Rx.parsePacket() > 0 ){
+    digitalWrite( LED2_PIN, HIGH);
+    rcvWiFi();
+    if(WiFibuff[0] == 'H'){
+      digitalWrite( LED_PIN, HIGH);
+      sendWiFi("2");
+    }
+  }
+
+  delay(5000);
+  sendWiFi("1");
+  digitalWrite( LED2_PIN, LOW);
+  digitalWrite( LED_PIN, LOW);
+  
+  end_loop();
+}
+
+void end_loop(){
+  
+  if (WiFi.status() != WL_CONNECTED) {
+    
+    WiFi.disconnect();
+    connectWiFi();
+  }
+}
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+#include "wifiConfig.h"
+
+const char ssid[] = MY_SSID; // SSID
+const char pass[] = MY_SSID_PASSWORD;  // password
+
+WiFiUDP wifiUdp; 
+static const char *kRemoteIpadr = "192.168.4.1";  //送信先のIPアドレス
+static const int kRmoteUdpPort = 10000; //送信先のポート
+
+const IPAddress ip(192, 168, 4, 2);       // IPアドレス(ゲートウェイも兼ねる)
+const IPAddress defge(192, 168, 4, 1);       // IPアドレス(ゲートウェイも兼ねる)
+const IPAddress subnet(255, 255, 255, 0); // サブネットマスク
+
+static void WiFi_setup()
+{
+  static const int kLocalPort = 5000;  //自身のポート
+  WiFi.mode(WIFI_STA);//重要!
+  WiFi.begin(ssid, pass);
+  delay(100);
+  WiFi.config(ip, defge, subnet);
+  while( WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.print("AP IP address: ");
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.println(myIP);
+
+  Serial.println("Starting UDP");
+  wifiUdp.begin(kLocalPort);
+}
+
+static void Serial_setup()
+{
+  Serial.begin(115200);
+  Serial.println(""); // to separate line  
+}
+
+void setup() {
+  Serial_setup();
+  WiFi_setup();
+}
+
+void loop() 
+{
+  if (wifiUdp.parsePacket()) {
+    char i = wifiUdp.read();  //ceramie追記 askiiから文字列へ
+    wifiUdp.flush();
+    Serial.println(i); // UDP通信で来た値を表示
+  }
 }
--- src/leaderDev/leaderDev.ino
+++ src/leaderDev/leaderDev.ino
@@ -1,9 +1,170 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+//#include <WiFiClient.h> 
+#include "wifiConfig.h"
 
+#define SW_PIN 13
+#define LED_PIN 4
+#define LED2_PIN 2
+
+char WiFibuff[16];
+int flag = 0;
+int led_state = LOW;
+
+// UDP setting
+WiFiUDP UDP_Tx;
+WiFiUDP UDP_Rx;
+
+// WiFi setting
+IPAddress myIP(LEADER_IP_ADDRESS);
+IPAddress mySubnet(MY_SUBNETMASK);
+IPAddress followerIpAddress1(FOLLOWER_IP_ADDRESS_1);
+const char *ssid = MY_SSID;
+const char *password = MY_SSID_PASSWORD;
+
+void connectWiFi() {
+
+  Serial.println("3");
+//  WiFi.begin( ssid, password);
+  Serial.println("4");
+  WiFi.config( myIP, WiFi.gatewayIP(), mySubnet);
+  Serial.println("5");
+
+//  while( WiFi.status() != WL_CONNECTED ){
+//    Serial.println("6");
+//    delay(500);
+//    led_state = !led_state;
+//    digitalWrite( LED_PIN, led_state);
+//  }
+  Serial.println("7");
+//  digitalWrite( LED_PIN, LOW);
+
+  Serial.println("8");
+  UDP_Tx.begin( LEADER_PORT_TX1 );
+  Serial.println("9");
+  UDP_Rx.begin( LEADER_PORT_RX1 );
+  Serial.println("10");
+}
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("start");
+  WiFi.mode( WIFI_AP ); // APを立てつつ、STAモード
+  Serial.println("1");
+  WiFi.softAP( ssid, password);
+  Serial.println("2");
+  connectWiFi();
+
+  Serial.println("11");
+//  pinMode( SW_PIN, INPUT);
+//  pinMode( LED_PIN, OUTPUT);
+//  digitalWrite( LED_PIN, HIGH);
+//  pinMode( LED2_PIN, OUTPUT);
+//  digitalWrite( LED2_PIN, HIGH);
+
+  delay(3000);
+
+//  digitalWrite( LED_PIN, LOW);
+//  digitalWrite( LED2_PIN, LOW);
+}
+
+void rcvWiFi() {
+  UDP_Rx.read(WiFibuff, 1);
+  UDP_Rx.flush();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+//  if( digitalRead( SW_PIN ) == HIGH && flag == 0 ){
+//    digitalWrite( LED_PIN, HIGH);
+//    if( UDP_Tx.beginPacket( followerIpAddress1, FOLLOWER_PORT_RX) ){
+//      UDP_Tx.write("H");
+//      UDP_Tx.endPacket();
+//      digitalWrite( LED2_PIN, HIGH);
+//      flag = 1;
+//    }
+//  }
+
+//  if(Serial.available() > 0) { 
+//    char data[2];
+//    data[0] = Serial.read();
+//    data[1] = '\0';
+//    if( UDP_Tx.beginPacket( followerIpAddress1, FOLLOWER_PORT_RX) ){
+//      UDP_Tx.write(data);
+//      UDP_Tx.endPacket();
+//      digitalWrite( LED2_PIN, HIGH);
+//      flag = 1;
+//    }
+//  } 
+  
+  if(UDP_Rx.parsePacket() > 0){
+    
+    rcvWiFi();
+    Serial.println("11");
+    if(WiFibuff[0] == '1'){
+      Serial.println("12");
+      //digitalWrite( LED2_PIN, HIGH);
+    }
+    else if( WiFibuff[0] == '2' ){
+      Serial.println("13");
+    }
+  }
+  //Serial.println(".");
+//  end_loop();
+}
+
+void end_loop(){
+  
+  if (WiFi.status() != WL_CONNECTED) {
+    
+    WiFi.disconnect();
+    connectWiFi();
+  }
+}
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <WiFiUDP.h>
+#include "wifiConfig.h"
+
+const char ssid[] = MY_SSID; // SSID
+const char pass[] = MY_SSID_PASSWORD;  // password
+const int localPort = 10000;      // 受信ポート番号
+
+const IPAddress ip(192, 168, 4, 1);       // IPアドレス(ゲートウェイも兼ねる)
+const IPAddress subnet(255, 255, 255, 0); // サブネットマスク
+
+WiFiUDP udp;
+static const char *kRemoteIpadr = "192.168.4.2";  //送信先のIPアドレス
+static const int kRmoteUdpPort = 5000; //送信先のポート
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, pass);           // SSIDとパスの設定
+  delay(100);                        // 追記：このdelayを入れないと失敗する場合がある
+  WiFi.softAPConfig(ip, ip, subnet); // IPアドレス、ゲートウェイ、サブネットマスクの設定
+
+  Serial.print("AP IP address: ");
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.println(myIP);
+
+  Serial.println("Starting UDP");
+  udp.begin(localPort);  // UDP通信の開始(引数はポート番号)
+
+  Serial.print("Local port: ");
+  Serial.println(localPort);
+}
+
+void loop() {
+  udp.beginPacket(kRemoteIpadr, kRmoteUdpPort);
+  udp.write('a');  //10進数のaskiiで送信される
+  udp.endPacket();
+
+  delay(3000);
 }
