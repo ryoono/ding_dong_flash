908ac1bb14ad60cb271aa207e0fdc2fbf4b18c68
ryoono <ono@pluslab.org>
2021-11-11 00:13:41 +0900

    3台繋げて通信確認成功

--- src/followerDev/followerDev.ino
+++ src/followerDev/followerDev.ino
@@ -1,172 +1,197 @@
-/*#include <ESP8266WiFi.h>
-//#include <WiFiClient.h> 
-#include <WiFiUDP.h>
-
-#include "wifiConfig.h"
-
-#define LED_PIN 4
-#define LED2_PIN 2
-
-char WiFibuff[16];
-int led_state = LOW;
-
-// UDP setting
-WiFiUDP UDP_Tx;
-WiFiUDP UDP_Rx;
-
-// WiFi settings
-IPAddress myIP( FOLLOWER_IP_ADDRESS_1 );
-IPAddress mySubnet( MY_SUBNETMASK );
-IPAddress leaderIpAddress1( LEADER_IP_ADDRESS );
-const char *ssid = MY_SSID;
-const char *password = MY_SSID_PASSWORD;
-
-int flag = 0;
-
-void rcvWiFi() {
-  UDP_Rx.read(WiFibuff, 1);
-  UDP_Rx.flush();
-}
-
-void connectWiFi() {
-  
-  WiFi.begin( ssid, password);
-  WiFi.config( myIP, WiFi.gatewayIP(), mySubnet);
-
-  while( WiFi.status() != WL_CONNECTED ){
-    delay(500);
-    led_state = !led_state;
-    digitalWrite( LED_PIN, led_state);
-  }
-  
-  digitalWrite( LED_PIN, LOW);
-
-  UDP_Tx.begin( FOLLOWER_PORT_TX );
-  UDP_Rx.begin( FOLLOWER_PORT_RX );
-}
-
-void setup() {
-
-  pinMode( LED_PIN, OUTPUT);
-  digitalWrite( LED_PIN, led_state);
-  pinMode( LED2_PIN, OUTPUT);
-  digitalWrite( LED2_PIN, LOW);
-  
-  WiFi.mode( WIFI_STA );
-  connectWiFi();
-}
-
-void sendWiFi(char byteData[]) {
-  
-  if ( UDP_Tx.beginPacket( leaderIpAddress1, LEADER_PORT_RX1) ) {
-    UDP_Tx.write(byteData);
-    UDP_Tx.endPacket();
-  }
-}
-
-void loop() {
-
-  if( UDP_Rx.parsePacket() > 0 ){
-    digitalWrite( LED2_PIN, HIGH);
-    rcvWiFi();
-    if(WiFibuff[0] == 'H'){
-      digitalWrite( LED_PIN, HIGH);
-      sendWiFi("2");
-    }
-  }
-
-  delay(5000);
-  sendWiFi("1");
-  digitalWrite( LED2_PIN, LOW);
-  digitalWrite( LED_PIN, LOW);
-  
-  end_loop();
-}
-
-void end_loop(){
-  
-  if (WiFi.status() != WL_CONNECTED) {
-    
-    WiFi.disconnect();
-    connectWiFi();
-  }
-}
-*/
-
+/*
+ * ピンポンフラッシュのデバイス子機
+ */
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
 #include "wifiConfig.h"
 
-const char ssid[] = MY_SSID; // SSID
-const char pass[] = MY_SSID_PASSWORD;  // password
+// LEDのI/Oポート設定
+#define LED_R 2
+#define LED_Y 4
+
+// ハイパワーLEDの点滅間隔 / 点滅期間
+#define HLED_LIGHTING_INTERVAL  500   // 間隔：500[ms]
+#define HLED_LIGHTING_COUNTER   60    // 期間：0.5[s]*60 = 30[s]
+
+// 書き込むデバイス子機に対応する定数をコメントアウトしてコンパイルする
+//#define FOLLOWER1
+//#define FOLLOWER2
+#define FOLLOWER3
+
+const char ssid[] = MY_SSID;          // SSID
+const char pass[] = MY_SSID_PASSWORD; // password
 
 WiFiUDP udp_Tx;
 WiFiUDP udp_Rx;
- 
-//static const char *kRemoteIpadr = "192.168.4.1";  //送信先のIPアドレス
-//static const int kRmoteUdpPort = 10000; //送信先のポート
 
-static const IPAddress leader_IP(LEADER_IP_ADDRESS);  //送信先のIPアドレス
-static const int port_Rx = FOLLOWER_PORT_RX; //受信用のポート
-static const int reader_1_Port_Rx = LEADER_PORT_RX1; //送信先のポート
+// この書き方あんまりよくない気がする
+// デバイスの数が増えたら管理が大変になる
+#ifdef FOLLOWER1
+// 自機の情報
+static const char id = '1';
+// このデバイスのネットワーク情報
+static const IPAddress ip( FOLLOWER_IP_ADDRESS_1 ); // IPアドレス(ゲートウェイも兼ねる)
+// 親機のネットワーク情報
+static const int reader_Port_Rx = LEADER_PORT_RX1;    //送信先のポート
+#endif
+#ifdef FOLLOWER2
+static const char id = '2';
+static const IPAddress ip( FOLLOWER_IP_ADDRESS_2 ); // IPアドレス(ゲートウェイも兼ねる)
+static const int reader_Port_Rx = LEADER_PORT_RX2;    //送信先のポート
+#endif
+#ifdef FOLLOWER3
+static const char id = '3';
+static const IPAddress ip( FOLLOWER_IP_ADDRESS_3 ); // IPアドレス(ゲートウェイも兼ねる)
+static const int reader_Port_Rx = LEADER_PORT_RX3;    //送信先のポート
+#endif
+
+// このデバイスのネットワーク情報
+static const IPAddress subnet( MY_SUBNETMASK );     // サブネットマスク
 static const int port_Tx = FOLLOWER_PORT_TX; //送信用のポート
+static const int port_Rx = FOLLOWER_PORT_RX; //受信用のポート
 
-const IPAddress ip(FOLLOWER_IP_ADDRESS_1);       // IPアドレス(ゲートウェイも兼ねる)
-const IPAddress defge(LEADER_IP_ADDRESS);       // IPアドレス(ゲートウェイも兼ねる)
-const IPAddress subnet(MY_SUBNETMASK); // サブネットマスク
+// 親機のネットワーク情報
+static const IPAddress leader_IP(LEADER_IP_ADDRESS);  //送信先のIPアドレス
 
-int led4;
-int led2;
+// HIGH：LED ON / LOW：LED OFF
+int ledY_sta;
+int ledR_sta;
 
-static void WiFi_setup()
-{
-  WiFi.mode(WIFI_STA);//重要!
+// ハイパワーLEDの点滅間隔 / 点滅回数
+int HLED_lighting_interval;
+int HLED_lighting_counter;
+
+void WiFi_setup(){
+
+  // 子機モードでAPにアクセスする
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
-  delay(100);
-  WiFi.config(ip, defge, subnet);
-  while( WiFi.status() != WL_CONNECTED) {
-    led4 = !led4;
-    digitalWrite( 4, led4);
+  delay(100); // 設定反映待ち
+  
+  WiFi.config(ip, ip, subnet);
+  while( WiFi.status() != WL_CONNECTED ){
+    
+    ledY_sta = !ledY_sta;
+    digitalWrite( LED_Y, ledY_sta);
     delay(500);
-    Serial.println(".");
+//    Serial.println(".");
   }
-  digitalWrite( 4, HIGH);
-  Serial.print("AP IP address: ");
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.println(myIP);
+  ledY_sta = HIGH;
+  digitalWrite( LED_Y, ledY_sta);
+//  Serial.print("AP IP address: ");
+//  IPAddress myIP = WiFi.softAPIP();
+//  Serial.println(myIP);
 
-  Serial.println("Starting UDP");
-  udp_Tx.begin(port_Tx);  // UDP通信の開始(引数はポート番号)
-  udp_Rx.begin(port_Rx);  // UDP通信の開始(引数はポート番号)
+//  Serial.println("Starting UDP");
+  udp_Tx.begin( port_Tx );  // UDP通信の開始(引数はポート番号)
+  udp_Rx.begin( port_Rx );  // UDP通信の開始(引数はポート番号)
 }
 
-static void Serial_setup()
-{
-  Serial.begin(115200);
-  Serial.println(""); // to separate line  
+//static void Serial_setup()
+//{
+//  Serial.begin(115200);
+//  Serial.println(""); // to separate line  
+//}
+
+// IOポートの初期設定
+void IO_setup(){
+  
+  ledY_sta = HIGH;
+  ledR_sta = LOW;
+  pinMode( LED_Y, OUTPUT);
+  pinMode( LED_R, OUTPUT);
+  digitalWrite( LED_Y, ledY_sta);
+  digitalWrite( LED_R, ledR_sta);
+}
+
+// ハイパワーLEDの初期設定
+void HLED_setup(){
+
+  HLED_lighting_interval  = 0;
+  HLED_lighting_counter   = 0;
+  // ハイパワーLED消灯処理
 }
 
 void setup() {
-  pinMode( 4, OUTPUT);
-  pinMode( 2, OUTPUT);
-  led4 = LOW;
-  led2 = LOW;
-  digitalWrite( 4, led4);
-  digitalWrite( 2, led2);
-  Serial_setup();
-  WiFi_setup();
+
+  // I/Oポートの初期設定
+  IO_setup();   // I/Oポートの初期設定
+  // ハイパワーLEDの初期設定
+  HLED_setup();
+//  Serial_setup();
+  WiFi_setup(); // ネットワークの初期設定
 }
 
 void loop() 
 {
-  if (udp_Rx.parsePacket()) {
-    led2 = !led2;
-    digitalWrite( 2, led2);
-    char i = udp_Rx.read();  //ceramie追記 askiiから文字列へ
+
+  // 点灯リクエストを受信したときの処理
+  if( udp_Rx.parsePacket() ){
+    
+//    ledR_sta = !ledR_sta;
+//    digitalWrite( 2, ledR_sta);
+    char data = udp_Rx.read();
+    // 受信データバッファクリア(ってリファレンスに書いてあるけど、実際はクリアされてない気がする)
     udp_Rx.flush();
-    udp_Tx.beginPacket(leader_IP, reader_1_Port_Rx);
-    udp_Tx.write(i);  //10進数のaskiiで送信される
-    udp_Tx.endPacket();
-    Serial.println(i); // UDP通信で来た値を表示
+
+    // 自機に向けられたデータを受信したら
+    // ハイパワーLEDを光らせる処理に入る
+    if( data == id ){
+
+      // ACKの送信
+      udp_Tx.beginPacket(leader_IP, reader_Port_Rx);
+      udp_Tx.write( id );
+      udp_Tx.endPacket();
+
+      // ハイパワーLED点灯準備
+      HLED_lighting_interval  = HLED_LIGHTING_INTERVAL;
+      HLED_lighting_counter   = HLED_LIGHTING_COUNTER;
+      
+//      Serial.println(i); // UDP通信で来た値を表示
+    }
+  }
+
+  // 指定回数点滅させる
+  if( HLED_lighting_counter > 0 ){
+
+    HLED_lighting_interval--;
+
+    // HLED_LIGHTING_INTERVAL秒間経過した場合、指定回数を更新
+    if( HLED_lighting_interval <= 0 ){
+
+      // カウンタを減らして、インターバルを初期値に戻す
+      HLED_lighting_counter--;
+      HLED_lighting_interval = HLED_LIGHTING_INTERVAL;
+      
+      // ハイパワーLEDの出力を反転させる
+      // ここに記載
+      ledR_sta = !ledR_sta;
+      digitalWrite( LED_R, ledR_sta);
+    }
+  }
+  else{
+    ledR_sta = LOW;
+    digitalWrite( LED_R, ledR_sta);
+    HLED_setup();
+  }
+  
+
+  // WiFiの接続チェック
+  // 毎周期確認し、切断時には再度繋ぎなおす
+  WiFi_connection_chk();
+  delay(1);
+}
+
+// WiFiの接続チェック
+// 切断時には再度繋ぎなおす
+void WiFi_connection_chk(){
+  
+  if( WiFi.status() != WL_CONNECTED ){
+    
+    WiFi.disconnect();
+//    Serial.println("disconnect!");
+    // ハイパワーLEDが点灯した状態でWiFiチェックが入ると、ずっと点灯してしまうためリセット
+    HLED_setup();
+    WiFi_setup();
   }
 }
--- src/leaderDev/leaderDev.ino
+++ src/leaderDev/leaderDev.ino
@@ -1,135 +1,19 @@
 /*
-#include <ESP8266WiFi.h>
-#include <WiFiUDP.h>
-//#include <WiFiClient.h> 
-#include "wifiConfig.h"
-
-#define SW_PIN 13
-#define LED_PIN 4
-#define LED2_PIN 2
-
-char WiFibuff[16];
-int flag = 0;
-int led_state = LOW;
-
-// UDP setting
-WiFiUDP UDP_Tx;
-WiFiUDP UDP_Rx;
-
-// WiFi setting
-IPAddress myIP(LEADER_IP_ADDRESS);
-IPAddress mySubnet(MY_SUBNETMASK);
-IPAddress followerIpAddress1(FOLLOWER_IP_ADDRESS_1);
-const char *ssid = MY_SSID;
-const char *password = MY_SSID_PASSWORD;
-
-void connectWiFi() {
-
-  Serial.println("3");
-//  WiFi.begin( ssid, password);
-  Serial.println("4");
-  WiFi.config( myIP, WiFi.gatewayIP(), mySubnet);
-  Serial.println("5");
-
-//  while( WiFi.status() != WL_CONNECTED ){
-//    Serial.println("6");
-//    delay(500);
-//    led_state = !led_state;
-//    digitalWrite( LED_PIN, led_state);
-//  }
-  Serial.println("7");
-//  digitalWrite( LED_PIN, LOW);
-
-  Serial.println("8");
-  UDP_Tx.begin( LEADER_PORT_TX1 );
-  Serial.println("9");
-  UDP_Rx.begin( LEADER_PORT_RX1 );
-  Serial.println("10");
-}
-
-void setup() {
-
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("start");
-  WiFi.mode( WIFI_AP ); // APを立てつつ、STAモード
-  Serial.println("1");
-  WiFi.softAP( ssid, password);
-  Serial.println("2");
-  connectWiFi();
-
-  Serial.println("11");
-//  pinMode( SW_PIN, INPUT);
-//  pinMode( LED_PIN, OUTPUT);
-//  digitalWrite( LED_PIN, HIGH);
-//  pinMode( LED2_PIN, OUTPUT);
-//  digitalWrite( LED2_PIN, HIGH);
-
-  delay(3000);
-
-//  digitalWrite( LED_PIN, LOW);
-//  digitalWrite( LED2_PIN, LOW);
-}
-
-void rcvWiFi() {
-  UDP_Rx.read(WiFibuff, 1);
-  UDP_Rx.flush();
-}
-
-void loop() {
-
-//  if( digitalRead( SW_PIN ) == HIGH && flag == 0 ){
-//    digitalWrite( LED_PIN, HIGH);
-//    if( UDP_Tx.beginPacket( followerIpAddress1, FOLLOWER_PORT_RX) ){
-//      UDP_Tx.write("H");
-//      UDP_Tx.endPacket();
-//      digitalWrite( LED2_PIN, HIGH);
-//      flag = 1;
-//    }
-//  }
-
-//  if(Serial.available() > 0) { 
-//    char data[2];
-//    data[0] = Serial.read();
-//    data[1] = '\0';
-//    if( UDP_Tx.beginPacket( followerIpAddress1, FOLLOWER_PORT_RX) ){
-//      UDP_Tx.write(data);
-//      UDP_Tx.endPacket();
-//      digitalWrite( LED2_PIN, HIGH);
-//      flag = 1;
-//    }
-//  } 
-  
-  if(UDP_Rx.parsePacket() > 0){
-    
-    rcvWiFi();
-    Serial.println("11");
-    if(WiFibuff[0] == '1'){
-      Serial.println("12");
-      //digitalWrite( LED2_PIN, HIGH);
-    }
-    else if( WiFibuff[0] == '2' ){
-      Serial.println("13");
-    }
-  }
-  //Serial.println(".");
-//  end_loop();
-}
-
-void end_loop(){
-  
-  if (WiFi.status() != WL_CONNECTED) {
-    
-    WiFi.disconnect();
-    connectWiFi();
-  }
-}
-*/
+ * ピンポンフラッシュのデバイス親機
+ */
 
 #include <ESP8266WiFi.h>
-//#include <WiFiClient.h> 
 #include <WiFiUDP.h>
 #include "wifiConfig.h"
+
+// ハイパワーLEDの点滅間隔 / 点滅期間
+#define HLED_LIGHTING_INTERVAL  500   // 間隔：500[ms]
+#define HLED_LIGHTING_COUNTER   60    // 期間：0.5[s]*60 = 30[s]
+
+// LEDのI/Oポート設定
+#define LED_R 2
+#define LED_Y 4
+#define SA 13
 
 const char ssid[] = MY_SSID; // SSID
 const char pass[] = MY_SSID_PASSWORD;  // password
@@ -137,18 +21,53 @@ const char pass[] = MY_SSID_PASSWORD;  // password
 const IPAddress ip(LEADER_IP_ADDRESS);       // IPアドレス(ゲートウェイも兼ねる)
 const IPAddress subnet(MY_SUBNETMASK); // サブネットマスク
 
-WiFiUDP udp_Tx;
-WiFiUDP udp_Rx;
+WiFiUDP udp_Tx_1;
+WiFiUDP udp_Rx_1;
+WiFiUDP udp_Tx_2;
+WiFiUDP udp_Rx_2;
+WiFiUDP udp_Tx_3;
+WiFiUDP udp_Rx_3;
 
 const int port_1_Rx = LEADER_PORT_RX1;      // 受信ポート番号
+const int port_2_Rx = LEADER_PORT_RX2;      // 受信ポート番号
+const int port_3_Rx = LEADER_PORT_RX3;      // 受信ポート番号
 
-//static const char *kRemoteIpadr = "192.168.4.2";  //送信先のIPアドレス
 static const IPAddress follower_1_IP(FOLLOWER_IP_ADDRESS_1);  //送信先のIPアドレス
-//static const int kRmoteUdpPort = 5000; //送信先のポート
+static const IPAddress follower_2_IP(FOLLOWER_IP_ADDRESS_2);  //送信先のIPアドレス
+static const IPAddress follower_3_IP(FOLLOWER_IP_ADDRESS_3);  //送信先のIPアドレス
 static const int port_1_Tx = LEADER_PORT_TX1; //送信用のポート
+static const int port_2_Tx = LEADER_PORT_TX2; //送信用のポート
+static const int port_3_Tx = LEADER_PORT_TX3; //送信用のポート
 static const int follower_1_Port_Rx = FOLLOWER_PORT_RX; //送信先のポート
+static const int follower_2_Port_Rx = FOLLOWER_PORT_RX; //送信先のポート
+static const int follower_3_Port_Rx = FOLLOWER_PORT_RX; //送信先のポート
+
+// ハイパワーLEDの点滅間隔 / 点滅回数
+int HLED_lighting_interval;
+int HLED_lighting_counter;
+
+// HIGH：LED ON / LOW：LED OFF
+int ledY_sta;
+int ledR_sta;
+
+// ハイパワーLEDの初期設定
+void HLED_setup(){
+
+  HLED_lighting_interval  = 0;
+  HLED_lighting_counter   = 0;
+  // ハイパワーLED消灯処理
+}
 
 void setup() {
+
+  ledY_sta = HIGH;
+  ledR_sta = LOW;
+  pinMode( LED_Y, OUTPUT);
+  pinMode( LED_R, OUTPUT);
+  pinMode( SA, INPUT);
+  digitalWrite( LED_Y, ledY_sta);
+  digitalWrite( LED_R, ledR_sta);
+  
   Serial.begin(115200);
   delay(100);
 
@@ -162,27 +81,84 @@ void setup() {
   Serial.println(myIP);
 
   Serial.println("Starting UDP");
-  udp_Tx.begin(port_1_Tx);  // UDP通信の開始(引数はポート番号)
-  udp_Rx.begin(port_1_Rx);  // UDP通信の開始(引数はポート番号)
+  udp_Tx_1.begin(port_1_Tx);  // UDP通信の開始(引数はポート番号)
+  udp_Rx_1.begin(port_1_Rx);  // UDP通信の開始(引数はポート番号)
+  udp_Tx_2.begin(port_2_Tx);  // UDP通信の開始(引数はポート番号)
+  udp_Rx_2.begin(port_2_Rx);  // UDP通信の開始(引数はポート番号)
+  udp_Tx_3.begin(port_3_Tx);  // UDP通信の開始(引数はポート番号)
+  udp_Rx_3.begin(port_3_Rx);  // UDP通信の開始(引数はポート番号)
 
   Serial.print("Local port: ");
   //Serial.println(localPort);
+
+  HLED_setup();
 }
 
 void loop() {
   
-  if (udp_Rx.parsePacket()) {
-    char i = udp_Rx.read();  //ceramie追記 askiiから文字列へ
-    udp_Rx.flush();
+  if (udp_Rx_1.parsePacket()) {
+    char i = udp_Rx_1.read();  //ceramie追記 askiiから文字列へ
+    udp_Rx_1.flush();
+    Serial.print("back : ");
+    Serial.println(i); // UDP通信で来た値を表示
+  }
+  if (udp_Rx_2.parsePacket()) {
+    char i = udp_Rx_2.read();  //ceramie追記 askiiから文字列へ
+    udp_Rx_2.flush();
+    Serial.print("back : ");
+    Serial.println(i); // UDP通信で来た値を表示
+  }
+  if (udp_Rx_3.parsePacket()) {
+    char i = udp_Rx_3.read();  //ceramie追記 askiiから文字列へ
+    udp_Rx_3.flush();
     Serial.print("back : ");
     Serial.println(i); // UDP通信で来た値を表示
   }
 
-  if( Serial.available() > 0 ){ 
+  //if( Serial.available() > 0 ){ 
+  if( digitalRead( SA ) == HIGH ){
     char data = Serial.read();
     Serial.println(data);
-    udp_Tx.beginPacket(follower_1_IP, follower_1_Port_Rx);
-    udp_Tx.write(data);  //10進数のaskiiで送信される
-    udp_Tx.endPacket();
+    udp_Tx_1.beginPacket(follower_1_IP, follower_1_Port_Rx);
+    udp_Tx_1.write('1');  //10進数のaskiiで送信される
+    udp_Tx_1.endPacket();
+
+    udp_Tx_2.beginPacket(follower_2_IP, follower_2_Port_Rx);
+    udp_Tx_2.write('2');  //10進数のaskiiで送信される
+    udp_Tx_2.endPacket();
+
+    udp_Tx_3.beginPacket(follower_3_IP, follower_3_Port_Rx);
+    udp_Tx_3.write('3');  //10進数のaskiiで送信される
+    udp_Tx_3.endPacket();
+
+    // ハイパワーLED点灯準備
+      HLED_lighting_interval  = HLED_LIGHTING_INTERVAL;
+      HLED_lighting_counter   = HLED_LIGHTING_COUNTER;
   }
+
+  // 指定回数点滅させる
+  if( HLED_lighting_counter > 0 ){
+
+    HLED_lighting_interval--;
+
+    // HLED_LIGHTING_INTERVAL秒間経過した場合、指定回数を更新
+    if( HLED_lighting_interval <= 0 ){
+
+      // カウンタを減らして、インターバルを初期値に戻す
+      HLED_lighting_counter--;
+      HLED_lighting_interval = HLED_LIGHTING_INTERVAL;
+      
+      // ハイパワーLEDの出力を反転させる
+      // ここに記載
+      ledR_sta = !ledR_sta;
+      digitalWrite( LED_R, ledR_sta);
+    }
+  }
+  else{
+    ledR_sta = LOW;
+    digitalWrite( LED_R, ledR_sta);
+    HLED_setup();
+  }
+  
+  delay(1);
 }
