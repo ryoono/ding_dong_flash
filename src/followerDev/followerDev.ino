e3c8fa25614867284d13f0bc9e14fc517b2184c1
ryoono <ono@pluslab.org>
2021-11-15 00:03:37 +0900

    状態遷移図をコードに反映(コンパイルOK)

--- src/followerDev/followerDev.ino
+++ src/followerDev/followerDev.ino
@@ -5,61 +5,56 @@
 #include <WiFiUDP.h>
 #include "wifiConfig.h"
 
+// 状態定義
+int state;
+#define LIGHTING_REQUEST_WAIT 0
+#define HIGH_POWER_LED_ON     10
+#define HIGH_POWER_LED_OFF    20
+
 // LEDのI/Oポート設定
 #define LED_R 2
 #define LED_Y 4
 
-// ハイパワーLEDの点滅間隔 / 点滅期間
-#define HLED_LIGHTING_INTERVAL  500   // 間隔：500[ms]
-#define HLED_LIGHTING_COUNTER   60    // 期間：0.5[s]*60 = 30[s]
+// ハイパワーLEDの点滅間隔
+int HLED_lighting_interval;
+#define HLED_LIGHTING_INTERVAL  500   // 間隔：300[ms]
 
 // 書き込むデバイス子機に対応する定数をコメントアウトしてコンパイルする
 //#define FOLLOWER1
 //#define FOLLOWER2
 #define FOLLOWER3
 
-const char ssid[] = MY_SSID;          // SSID
-const char pass[] = MY_SSID_PASSWORD; // password
-
-WiFiUDP udp_Tx;
-WiFiUDP udp_Rx;
-
 // この書き方あんまりよくない気がする
 // デバイスの数が増えたら管理が大変になる
 #ifdef FOLLOWER1
-// 自機の情報
+// 自機のID情報
 static const char id = '1';
-// このデバイスのネットワーク情報
-static const IPAddress ip( FOLLOWER_IP_ADDRESS_1 ); // IPアドレス(ゲートウェイも兼ねる)
-// 親機のネットワーク情報
-static const int reader_Port_Rx = LEADER_PORT_RX1;    //送信先のポート
+// このデバイスのipアドレス
+static const IPAddress ip( FOLLOWER_IP_ADDRESS_1 );
 #endif
 #ifdef FOLLOWER2
 static const char id = '2';
-static const IPAddress ip( FOLLOWER_IP_ADDRESS_2 ); // IPアドレス(ゲートウェイも兼ねる)
-static const int reader_Port_Rx = LEADER_PORT_RX2;    //送信先のポート
+static const IPAddress ip( FOLLOWER_IP_ADDRESS_2 );
 #endif
 #ifdef FOLLOWER3
 static const char id = '3';
-static const IPAddress ip( FOLLOWER_IP_ADDRESS_3 ); // IPアドレス(ゲートウェイも兼ねる)
-static const int reader_Port_Rx = LEADER_PORT_RX3;    //送信先のポート
+static const IPAddress ip( FOLLOWER_IP_ADDRESS_3 );
 #endif
 
 // このデバイスのネットワーク情報
 static const IPAddress subnet( MY_SUBNETMASK );     // サブネットマスク
-static const int port_Tx = FOLLOWER_PORT_TX; //送信用のポート
 static const int port_Rx = FOLLOWER_PORT_RX; //受信用のポート
 
-// 親機のネットワーク情報
-static const IPAddress leader_IP(LEADER_IP_ADDRESS);  //送信先のIPアドレス
+// AP情報
+const char ssid[] = MY_SSID;          // SSID
+const char pass[] = MY_SSID_PASSWORD; // password
+
+// UDP用のポート設定
+WiFiUDP udp_Rx;
 
 // HIGH：LED ON / LOW：LED OFF
 int ledY_sta;
 int ledR_sta;
-
-// ハイパワーLEDの点滅間隔 / 点滅回数
-int HLED_lighting_interval;
-int HLED_lighting_counter;
 
 void WiFi_setup(){
 
@@ -83,7 +78,6 @@ void WiFi_setup(){
 //  Serial.println(myIP);
 
 //  Serial.println("Starting UDP");
-  udp_Tx.begin( port_Tx );  // UDP通信の開始(引数はポート番号)
   udp_Rx.begin( port_Rx );  // UDP通信の開始(引数はポート番号)
 }
 
@@ -108,12 +102,14 @@ void IO_setup(){
 void HLED_setup(){
 
   HLED_lighting_interval  = 0;
-  HLED_lighting_counter   = 0;
   // ハイパワーLED消灯処理
 }
 
 void setup() {
 
+  // 状態の初期化
+  state = 0;
+  
   // I/Oポートの初期設定
   IO_setup();   // I/Oポートの初期設定
   // ハイパワーLEDの初期設定
@@ -125,56 +121,79 @@ void setup() {
 void loop() 
 {
 
-  // 点灯リクエストを受信したときの処理
-  if( udp_Rx.parsePacket() ){
-    
-//    ledR_sta = !ledR_sta;
-//    digitalWrite( 2, ledR_sta);
-    char data = udp_Rx.read();
-    // 受信データバッファクリア(ってリファレンスに書いてあるけど、実際はクリアされてない気がする)
-    udp_Rx.flush();
+  switch( state ){
 
-    // 自機に向けられたデータを受信したら
-    // ハイパワーLEDを光らせる処理に入る
-    if( data == id ){
+    // ############################
+    // ##### 点灯リクエスト待ち #####
+    case LIGHTING_REQUEST_WAIT:
 
-      // ACKの送信
-      udp_Tx.beginPacket(leader_IP, reader_Port_Rx);
-      udp_Tx.write( id );
-      udp_Tx.endPacket();
+      // UDPでデータを受信したときの処理
+      // 点灯リクエスト(自機のID)の場合、
+      if( udp_Rx.parsePacket() ){
+        
+        char data = udp_Rx.read();
+        // 受信データバッファクリア(ってリファレンスに書いてあるけど、実際はクリアされてない気がする)
+        udp_Rx.flush();
 
-      // ハイパワーLED点灯準備
-      HLED_lighting_interval  = HLED_LIGHTING_INTERVAL;
-      HLED_lighting_counter   = HLED_LIGHTING_COUNTER;
-      
-//      Serial.println(i); // UDP通信で来た値を表示
-    }
-  }
+        // 点灯リクエスト受信処理
+        // ハイパワーLED点灯に状態遷移
+        if( data == id ){
 
-  // 指定回数点滅させる
-  if( HLED_lighting_counter > 0 ){
+          // ハイパワーLED点灯処理
+          // ここに書く
+          ledR_sta = HIGH;
+          digitalWrite( LED_R, ledR_sta);
+          HLED_lighting_interval = 0; // タイマリセット
+          state = HIGH_POWER_LED_ON;
+        }
+        
+      }
+      break;
 
-    HLED_lighting_interval--;
+    // ############################
+    // ##### ハイパワーLED点灯 #####
+    case HIGH_POWER_LED_ON:
 
-    // HLED_LIGHTING_INTERVAL秒間経過した場合、指定回数を更新
-    if( HLED_lighting_interval <= 0 ){
+      // 指定時間後に、ハイパワーLED消灯へ遷移
+      if( ++HLED_lighting_interval >= HLED_LIGHTING_INTERVAL ){
 
-      // カウンタを減らして、インターバルを初期値に戻す
-      HLED_lighting_counter--;
-      HLED_lighting_interval = HLED_LIGHTING_INTERVAL;
-      
-      // ハイパワーLEDの出力を反転させる
-      // ここに記載
-      ledR_sta = !ledR_sta;
+        // ハイパワーLED消灯処理
+        // ここに書く
+        ledR_sta = LOW;
+        digitalWrite( LED_R, ledR_sta);
+        HLED_lighting_interval = 0; // タイマリセット
+        state = HIGH_POWER_LED_OFF;
+      }
+      break;
+
+    // ############################
+    // ##### ハイパワーLED消灯 #####
+    case HIGH_POWER_LED_OFF:
+
+      // 指定時間後に、点灯リクエスト待ちへ遷移
+      if( ++HLED_lighting_interval >= HLED_LIGHTING_INTERVAL ){
+
+        // ハイパワーLED消灯処理
+        // ここに書く
+        ledR_sta = LOW;
+        digitalWrite( LED_R, ledR_sta);
+        HLED_lighting_interval = 0; // タイマリセット
+        state = LIGHTING_REQUEST_WAIT;
+      }
+      break;
+
+    // ###################################
+    // ## 例外処理 点灯リクエスト待ちへ遷移 ##
+    default:
+      // どの状態でこの処理を行うのか分からないため、ハイパワーLED消灯処理
+      // ここに書く
+      // ハイパワーLED消灯処理
+      // ここに書く
+      ledR_sta = LOW;
       digitalWrite( LED_R, ledR_sta);
-    }
-  }
-  else{
-    ledR_sta = LOW;
-    digitalWrite( LED_R, ledR_sta);
-    HLED_setup();
-  }
-  
+      state = LIGHTING_REQUEST_WAIT;
+      break;
+  } 
 
   // WiFiの接続チェック
   // 毎周期確認し、切断時には再度繋ぎなおす
